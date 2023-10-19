90b119df2eaaafb5fbd753668ebeae8a738a1d8c
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-10-19 16:27:44 +0200

    fix bonus

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -22,24 +22,25 @@ class ComicsTableSeeder extends Seeder
 
         foreach ($_comics as $_comic) {
 
-            $comic = new Comic();
 
-            $comic->title = $_comic['title'];
-            $comic->description = $_comic['description'];
-            $comic->thumb = $_comic['thumb'];
-            $comic->price = $_comic['price'];
-            $comic->series = $_comic['series'];
-            $comic->sale_date = $_comic['sale_date'];
-            $comic->type = $_comic['type'];
+
+            //$comic->title = $_comic['title'];
+            // $comic->description = $_comic['description'];
+            // $comic->thumb = $_comic['thumb'];
+            // $comic->price = $_comic['price'];
+            //$comic->series = $_comic['series'];
+            // $comic->sale_date = $_comic['sale_date'];
+            // $comic->type = $_comic['type'];
 
 
             //fare unset per eliminare i 2 arrey, oppure scrivere tutto a mano
 
-            //  unset($_comic['artists']);
-            //unset($_comic['writers']);
+            unset($_comic['artists']);
+            unset($_comic['writers']);
 
+            $comic = new Comic();
 
-            // $comic->fill($_comic);
+            $comic->fill($_comic);
 
             $comic->save();
         }
