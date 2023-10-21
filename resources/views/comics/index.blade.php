1d1a5647006eb36f1018e53f69b4672cf3856628
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-10-21 20:00:37 +0200

    feat table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,72 +3,88 @@
 @section('main-content')
     <section class="container my-3">
 
-        <h1 class="my-2"> Mostra i Fumetti</h1>
+        <h1 class="my-2"> Lista dei Fumetti</h1>
 
         <a href={{ route('comics.create') }} class="btn btn-primary my-2">Aggiungi un nuovo Fumetto</a>
 
+        <table class="table table-striped">
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Id:</th>
+                        <th scope="col">Titolo:</th>
+                        <th scope="col">Serie:</th>
+                        <th scope="col">Genere:</th>
+                        <th scope="col">Data di acquisto:</th>
+                        <th scope="col">Prezzo:</th>
+                        <th scope="col">D-T-M:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->id }}</th>
+                            <td>{{ $comic->title }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td>{{ $comic->sale_date }}</td>
+                            <td>{{ $comic->price }}</td>
+                            <td>
+                                <a href={{ route('comics.show', $comic) }}><i class="fa-solid fa-eye"></i></a>
+                                <a href={{ route('comics.edit', $comic) }}><i
+                                        class="fa-solid text-warning fa-pen-to-square px-2"></i></a>
+                                <!-- Button trigger modal -->
+                                <a href="#" type="button" data-bs-toggle="modal"
+                                    data-bs-target="#delete-modal-{{ $comic->id }}">
+                                    <i class="fa-solid text-danger fa-trash-can"></i>
+                                </a>
+
+                            </td>
+                        </tr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="delete-modal-{{ $comic->id }}" tabindex="-1"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModal"> Vuoi eliminare
+                                            questo fumetto?
+                                        </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        {{ $comic->title }}
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Annulla</button>
 
 
-        @foreach ($comics as $comic)
-            <ul class="list-group mt-5 w-50">
+                                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-danger">Elimina</button>
 
-                <li class="list-group-item ">
-                    <strong>Id:</strong> {{ $comic->id }}
-                </li>
-                <li class="list-group-item d-flex">
+                                        </form>
 
-                    <div class="flex-grow-1"><strong>Titolo:</strong> {{ $comic->title }}</div>
-
-                    <a href={{ route('comics.show', $comic) }}><i class="fa-solid fa-eye"></i></a>
-
-                    <a href={{ route('comics.edit', $comic) }}><i
-                            class="fa-solid text-warning fa-pen-to-square px-2"></i></a>
-
-                    <!-- Button trigger modal -->
-                    <a href="#" type="button" data-bs-toggle="modal"
-                        data-bs-target="#delete-modal-{{ $comic->id }}">
-                        <i class="fa-solid text-danger fa-trash-can"></i>
-                    </a>
-
-                </li>
-                <li class="list-group-item">
-                    <strong>Descrizione:</strong> {{ $comic->description }}
-                </li>
-                <li class="list-group-item">
-                    <strong>Prezzo:</strong> {{ $comic->price }}
-                </li>
-            </ul>
-
-            <!-- Modal -->
-            <div class="modal fade" id="delete-modal-{{ $comic->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModal"> Vuoi eliminare
-                                questo fumetto?
-                            </h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            {{ $comic->title }}
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    @endforeach
+                </tbody>
+            </table>
 
 
-                            <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button class="btn btn-danger">Elimina</button>
+            <div class="d-flex flex-column">
+                <h5>*Legenda D-T-M</h5>
+                <span>Per vedere tutti i dettagli del fumetto: <i class="fa-solid text-primary fa-eye"></i></span>
+                <span>Per modificare un fumetto: <i class="fa-solid text-warning fa-pen-to-square px-2"></i></span>
+                <span>Per eliminare un fumetto: <i class="fa-solid text-danger fa-trash-can"></i></span>
 
-                            </form>
-
-                        </div>
-                    </div>
-                </div>
             </div>
-        @endforeach
 
     </section>
 @endsection
